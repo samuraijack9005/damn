ee153c4c3204194c5c452dd6a523cfb352f9f463
samuraijack9005 <41179530+samuraijack9005@users.noreply.github.com>
2018-07-21 22:28:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,6 @@ drive =webdriver.Chrome()
 time.sleep(15)
 drive.get("http://samurai9005.ooo")
 #git remote set-url origin ssh://git@github.com/derekerdmann/lunch_call.git
+#mv ~/.gitconfig ~/.gitconfig_old
 
+#git config --global user.name "Your Real Name Here" && git config --global user.email "Your Email Address Here"
